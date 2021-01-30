715f465fe5b765dafe689b2a42cd5534848c54fa
Chris Johnson <cjohn281@outlook.com>
2021-01-30 10:45:11 -0500

    SparkSession and base DataFrame are now built in Main and passed to the appropriate HashtagByRegion methods

--- HashtagByRegion/src/main/scala/main/Main.scala
+++ HashtagByRegion/src/main/scala/main/Main.scala
@@ -3,9 +3,14 @@ package main
 import org.apache.spark.sql.SparkSession
 
 import hashtagByRegion.HashtagByRegion
+import util.FileUtil
 
 object Main {
   def main(args: Array[String]) = {
+
+    // Original jsonPath = s3a://adam-king-848/data/twitter_data.json
+    // jsonPath currently points to test data
+    val jsonPath = "twitter_data.json"
 
     val spark = SparkSession
       .builder()
@@ -13,11 +18,13 @@ object Main {
       .master("local[4]")
       .getOrCreate()
 
+    val twitterDF = FileUtil.getDataFrameFromJson(spark, jsonPath)
+
     if (args.isEmpty) {
-      HashtagByRegion.getHashtagsByRegionAll(spark)
+      HashtagByRegion.getHashtagsByRegionAll(spark, twitterDF)
     } else {
       val region = args(0)
-      HashtagByRegion.getHashtagsByRegion(spark, region)
+      HashtagByRegion.getHashtagsByRegion(spark, twitterDF, region)
     }
   }
 }
